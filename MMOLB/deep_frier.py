2d12c3037953b04555ef454eab7c6ae5850eb76f
Henry Downs <110191518+henrywdowns@users.noreply.github.com>
2025-11-12 22:38:34 +0000

    interaction regression up and running

--- MMOLB/deep_frier.py
+++ MMOLB/deep_frier.py
@@ -12,25 +12,34 @@ class DeepFrier:
         self.league = league
         self.diff_threshold = diff_threshold
 
-    def _prepare_data(self, category, dependent_variable, independent_variables, scope, league_obj):
-        match category.lower():
-            case 'batting' | 'hitting':
-                category = 'batting'
-            case _:
-                category = category.lower()        
+    def _prepare_data(self, category, dependent_variable, independent_variables, scope, league_obj, merged_df_inject = None):
+        if merged_df_inject is None: # skip all this if passing in a merged df. right now purely for including interactions in regressions
+            # account for commonly interchangeable terminology
+            match category.lower():
+                case 'batting' | 'hitting':
+                    category = 'batting'
+                case _:
+                    category = category.lower()        
+            # default to class attributes
+            if not league_obj: league_obj = self.league
+            attrs_df = self._attributes_data
 
-        league_obj = league_obj or self.league
-        attrs_df = self._attributes_data
-        if not independent_variables:
-            independent_variables = self._cat_stat_dict(attrs_df)[category.capitalize()]
-        stats_df = self._stats_data[category]
-        attrs_filter = attrs_df[(attrs_df['category'].str.lower()==category) & (attrs_df['group']==scope)]
-        attrs_piv = attrs_filter.pivot_table(
-            index=['team','player','group','category','team_win_diff'],
-            columns='stat', values='value'
-        ).reset_index()
-        merged_df = attrs_piv.merge(stats_df,left_on=['player','team'],right_on=['player_name','team_name'])
+            # include all attributes in category by default, filter both datasets by category, filter by attributes scope (total, base, or equip)
+            if not independent_variables:
+                independent_variables = self._cat_stat_dict(attrs_df)[category.capitalize()]
+            stats_df = self._stats_data[category]
+            attrs_filter = attrs_df[(attrs_df['category'].str.lower()==category) & (attrs_df['group']==scope)]
+            
+            # pivot attrs data to give each independent variable its own col
+            attrs_piv = attrs_filter.pivot_table(
+                index=['team','player','group','category','team_win_diff'],
+                columns='stat', values='value'
+            ).reset_index()
 
+            # inner join
+            merged_df = attrs_piv.merge(stats_df,left_on=['player','team'],right_on=['player_name','team_name'])
+        else:
+            merged_df = merged_df_inject
         X = merged_df[independent_variables]
         y = merged_df[dependent_variable]
         return merged_df, X, y
@@ -60,7 +69,7 @@ class DeepFrier:
         
         return outputs
     
-    def _cat_stat_dict(self,df):
+    def _cat_stat_dict(self,df) -> dict:
         return {category: list(set(df.loc[df['category'] == category, 'stat'])) for category in df['category'].unique()}
 
 
@@ -103,17 +112,35 @@ class DeepFrier:
         }
     
     @with_sm_summary
-    def interaction_regression(self, category, dependent_variable: str, interaction_variables: list = [], scope='total'):        
+    def interaction_regression(self, category, dependent_variable: str, interaction_variables: list = [], scope='total'): 
+        from itertools import combinations
+        # initialize df variables
         merged_df, X, y = self._prepare_data(category, dependent_variable, [], scope, self.league)
-
-        base_independents = self._cat_stat_dict[category]
-        if False:
-            for item_x in interaction_variables:
-                for item_y in interaction_variables:
-                    if item_x
-
+        # iterate through the provided variables and create new columns for the products
+        pairs = list(combinations(interaction_variables,2))
+        inter_names = []
+        for a, b in pairs:
+            inter_name = f'{a}*{b}'
+            merged_df[inter_name] = merged_df[a]*merged_df[b]
+            inter_names.append(inter_name)
         
-    
+        # redefine the independent variables list to include the interactions
+        all_features = self._cat_stat_dict(self._attributes_data)[category.capitalize()] + inter_names
+        print(all_features)
+        X = merged_df[all_features]
+
+        # from here it's the same as before
+        X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=4)
+        model = LinearRegression()
+        model.fit(X_train,y_train)
+
+        return {
+            "model": model,
+            "X_train": X_train, "y_train": y_train,
+            "X_test": X_test,   "y_test": y_test,
+            "features": list(X.columns)
+        }
+
     def attrs_interaction(self, category, dependent_variable: str, independent_variables: list = [], scope='total',degree=2):
         from sklearn.preprocessing import PolynomialFeatures, StandardScaler
         from sklearn.pipeline import Pipeline
--- scripts/league_regression.py
+++ scripts/league_regression.py
@@ -5,9 +5,14 @@ from MMOLB import APIHandler, DeepFrier
 handler = APIHandler()
 liberty = handler.get_league(populate='all')
 frier = DeepFrier(liberty)
-batting_attrs = frier._cat_stat_dict('Batting')
-combos = []
 model = frier.attrs_regression('batting',dependent_variable='OBP',sm_summary=True,detailed_output=True)
 print(model['summary_text'])
 inters = frier.attrs_interaction('batting','OBP',['Insight','Determination','Discipline','Vision'])
 print(inters['feature_coefs'])
+
+inters_regression = frier.interaction_regression('batting',dependent_variable='OBP',
+                                                 interaction_variables=['Vision','Insight','Discipline','Determination'],
+                                                 sm_summary=True,detailed_output=True)
+
+
+print(inters_regression['summary_text'])
